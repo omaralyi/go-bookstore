8accb4832d582be88bcca29a3319ecafb41686af
omaralyi <omaraly@gmail.com>
2022-05-23 16:18:22 +0200

    minor updates

--- pkg/controllers/book-controller.go
+++ pkg/controllers/book-controller.go
@@ -1,1 +1,35 @@
 package controllers
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/gorilla/mux"
+	"github.com/omaralyi/go-bookstore/pkg/models"
+	"github.com/omaralyi/go-bookstore/pkg/utils"
+	"net/http"
+	"strconv"
+)
+
+var NewBook models.Book
+
+func GetBooks(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	response, _ := json.Marshal(newBooks)
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	bookID, err := strconv.ParseInt(params["BookId"], 0, 0)
+	if err != nil {
+		fmt.Println("error while parsing input")
+		panic(err)
+	}
+	book, _ := models.GetBookById(bookID)
+	response, _ := json.Marshal(book)
+	w.Header().Set("Content-type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(response)
+}
--- pkg/models/book.go
+++ pkg/models/book.go
@@ -8,8 +8,8 @@ import (
 var db *gorm.DB
 
 type Book struct {
-	gorm.model
-	Name        string `gorm:""json:"name"`
+	gorm.Model
+	Name        string `gorm:"" json:"name"`
 	Author      string `json:"author"`
 	Publication string `json:"publication"`
 }
@@ -27,9 +27,19 @@ func (b *Book) CreateBook() *Book {
 }
 
 func GetAllBooks() []Book {
-	var Books []Book
-	db.Find(&Books)
-	return Books
+	var books []Book
+	db.Find(&books)
+	return books
 }
 
-func GetBookById(Id int)
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db = db.Where("ID=?", Id).Find(getBook)
+	return &getBook, db
+}
+
+func DeleteBook(Id int64) Book {
+	var book Book
+	db.Where("ID=?", Id).Delete(book)
+	return book
+}
